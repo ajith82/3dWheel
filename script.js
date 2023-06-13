25dfac5d297e101f35264bf3f157f6c503e83170
ajith82 <ajith@sketchbrahma.com>
2023-06-13 17:25:29 +0530

    div direction fixed, doing 3d

---
+++ script.js
@@ -0,0 +1,9 @@
+const slideOne = document.getElementById("slideOne");
+const slideTwo = document.getElementById("slideTwo");
+const img = document.getElementsByTagName("img");
+
+// document.addEventListener("scroll", () => {
+//   for (let i = 0; i < img.length; i++) {
+//     img[i].style.transform = `rotateX( ${window.pageYOffset}deg)`;
+//   }
+// });
