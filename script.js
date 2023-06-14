dbfec00ca984ce2f093f42179852030b40e4b459
ajith82 <ajith@sketchbrahma.com>
2023-06-14 13:18:33 +0530

    scroll functionality completely done

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const section = document.getElementsByTagName("section")[0];
 const container = document.getElementsByClassName("container")[0];
 const slide = document.getElementsByClassName("slide")[0];
+console.log(window.pageYOffset);
+const styleSheet = document.styleSheets[0];
+
+if (window.pageYOffset === 0) {
+  const rotateKeyframesDefault = `@keyframes rotate {
+        0% {
+          transform: perspective(1000px) rotateX(0deg);
+        }
+        100% {
+          transform: perspective(1000px) rotateX(359deg);
+        }
+      }`;
+
+  styleSheet.insertRule(rotateKeyframesDefault, 0);
+}
+
 document.addEventListener("scroll", () => {
-  const scrollTop = window.pageYOffset;
+  let scrollTop = window.pageYOffset;
+  console.log(scrollTop);
   const rotateKeyframes = `@keyframes rotate {
         0% {
           transform: perspective(1000px) rotateX(${scrollTop}deg);
@@ -11,7 +28,7 @@ document.addEventListener("scroll", () => {
           transform: perspective(1000px) rotateX(${scrollTop + 359}deg);
         }
       }`;
-  const styleSheet = document.styleSheets[0];
+
   styleSheet.deleteRule(0);
   styleSheet.insertRule(rotateKeyframes, 0);
 });
