97cbadccf1e414d659c0cd4693693e0e96bc98a6
ajith82 <ajith@sketchbrahma.com>
2023-06-14 12:39:13 +0530

    added 2 wheels, scroll will work

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const slideOne = document.getElementById("slideOne");
-const slideTwo = document.getElementById("slideTwo");
-const img = document.getElementsByTagName("img");
-
-// document.addEventListener("scroll", () => {
-//   for (let i = 0; i < img.length; i++) {
-//     img[i].style.transform = `rotateX( ${window.pageYOffset}deg)`;
-//   }
-// });
+const section = document.getElementsByTagName("section")[0];
+const container = document.getElementsByClassName("container")[0];
+const slide = document.getElementsByClassName("slide")[0];
+document.addEventListener("scroll", () => {
+  const scrollTop = window.pageYOffset;
+  const rotateKeyframes = `@keyframes rotate {
+        0% {
+          transform: perspective(1000px) rotateX(${scrollTop}deg);
+        }
+        100% {
+          transform: perspective(1000px) rotateX(${scrollTop + 359}deg);
+        }
+      }`;
+  const styleSheet = document.styleSheets[0];
+  styleSheet.deleteRule(0);
+  styleSheet.insertRule(rotateKeyframes, 0);
+});
